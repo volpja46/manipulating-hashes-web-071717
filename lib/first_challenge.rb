db6f5201af25407310352be49444def26c8ac10b
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-13 12:00:23 -0400

    okdone

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,15 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+contacts.each do |person, info|
+    info.each do |key, value|
+        if key == :favorite_icecream_flavors
+            value.delete_if do |flavor|
+                flavor == "strawberry"
+                end
+            end
+        end
   contacts
+end
 end
 
